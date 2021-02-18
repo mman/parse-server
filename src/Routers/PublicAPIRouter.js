a5ebf56c2580bae8ccb507e94999fddb82135977
Martin Man <mman@martinman.net>
2021-02-18 14:36:22 +0100

    add ability to trigger the password reset flow by POST to /parse/apps/APP_ID/request_password_reset with username argument

--- src/Routers/PublicAPIRouter.js
+++ src/Routers/PublicAPIRouter.js
@@ -157,6 +157,25 @@ export class PublicAPIRouter extends PromiseRouter {
     const { username, new_password, token: rawToken } = req.body;
     const token = rawToken && typeof rawToken !== 'string' ? rawToken.toString() : rawToken;
 
+    if (username && (!token || !new_password) && req.xhr === false) {
+      return config.userController.sendPasswordResetEmail(username).then(
+        () => {
+          const params = qs.stringify({
+            id: config.applicationId,
+            username,
+            app: config.appName,
+          });
+          return Promise.resolve({
+            status: 302,
+            location: `${config.passwordResetInitiatedURL}?${params}`,
+          });
+        },
+        () => {
+          return this.invalidLink(req);
+        }
+      );
+    }
+
     if ((!username || !token || !new_password) && req.xhr === false) {
       return this.invalidLink(req);
     }
